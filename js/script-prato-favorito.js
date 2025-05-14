84bc0c4b1d77d28ff3db97a3c7933a1bce834506
Nikolas <nikolasrm@al.insper.edu.br>
2025-05-14 15:21:52 -0300

    arrumando o alinhamento e resposividade da página dispensa

--- js/script-prato-favorito.js
+++ js/script-prato-favorito.js
@@ -9,12 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
 
     })
-    EstrelaImg.addEventListener("mouseover", function(event){
-        EstrelaImg.style.borderColor = "#eb8b47"
-    })
-    EstrelaImg.addEventListener("mouseout", function(event){
-        EstrelaImg.style.borderColor = "#808080"
-    })
+
 
 
 
