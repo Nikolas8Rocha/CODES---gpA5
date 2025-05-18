85a16ef793b2c250fdb461fca711d47c5d4ee4a5
Nikolas <nikolasrm@al.insper.edu.br>
2025-05-18 11:08:17 -0300

    fazendo com que todos os botões da tela adcionar leve para a mesma tela.

--- js/script-adicionar.js
+++ js/script-adicionar.js
@@ -3,5 +3,20 @@ document.addEventListener('DOMContentLoaded', function(){
     iconeDispensa = document.querySelector('.dispensa');
     iconeDispensa.style.filter = 'invert(0%)';
 
+    botao_a = document.querySelectorAll(".a-lista")
+    //console.log(botao_a[0])
     
+    for (item of botao_a){
+        item.addEventListener("click", function(evento){
+            evento.preventDefault();
+
+            
+            item_clicado = evento.currentTarget;
+            console.log(item_clicado);
+            localStorage.setItem("ingrediente", item_clicado.querySelector(".texto-lista").innerHTML);
+            window.location.href = "/Item_a_ser_adicionado.html";
+        })
+    
+        
+    }
 })
