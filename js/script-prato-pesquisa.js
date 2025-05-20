1d8090140d7e7a98b3496b6af85ff990a69d3774
Nikolas <nikolasrm@al.insper.edu.br>
2025-05-20 11:53:51 -0300

    adicionando um novo css, html e js

---
+++ js/script-prato-pesquisa.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    //icone preto no footer
+    iconeRelogio = document.querySelector('.livro');
+    iconeRelogio.style.filter = 'invert(0%)';
+
+
+
+
+
+
+   
+    
+
+})
