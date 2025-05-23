186fdfbf4ed0b15d0c1bd1e4a284c3fbda5f969d
Nikolas <nikolasrm@al.insper.edu.br>
2025-05-23 11:45:54 -0300

    m

--- js/script-index.js
+++ js/script-index.js
@@ -3,24 +3,4 @@ document.addEventListener('DOMContentLoaded', function(){
     iconeCasa = document.querySelector('.casa');
     iconeCasa.style.filter = 'invert(0%)';
     
-    //fundo_semana = document.querySelector("#semana");
-
-    //lista_novi = []
-    /*
-    lista_semana = ["/Imagens/SemanaNovidade/viraPaulista.png",
-        "/Imagens/SemanaNovidade/bifearolle.png",
-        "/Imagens/SemanaNovidade/feijoada.png",
-        "/Imagens/SemanaNovidade/lasanha.png",
-        "/Imagens/SemanaNovidade/peixeempa.png"
-    ]
-    */
-    //cont = 0;
-
-    //function trocaISemana(){
-        //fundo_semana.src = lista_semana[cont];
-        //cont = (cont + 1) % lista_semana.lenght;
-    //}
-
-    //setInterval(trocaISemana, 5000);
-    //trocaISemana();
 })
